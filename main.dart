1c5dc43dffe29ba724097c1215d64cea9200b594
MaherabHossain <59120265+MaherabHossain@users.noreply.github.com>
2022-03-24 21:24:06 +0600

    update

--- main.dart
+++ main.dart
@@ -76,38 +76,3 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    final providerData = Provider.of<Data>(context);
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              providerData.count.toString(),
-              style: TextStyle(fontSize: 24),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                providerData.increment();
-              },
-              child: Text('Increment'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => School()),
-                );
-              },
-              child: Text('Go to School Screen'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
